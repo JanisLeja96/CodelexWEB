fffa9edc035793c7f97feadb472fef0af5d56f74
JanisLeja1996 <janis.leja1996@gmail.com>
2020-10-05 13:53:39 +0300

    Edit

--- GET_POST/index.php
+++ GET_POST/index.php
@@ -1,20 +1,25 @@
 <?php
 
-$min = $_POST['Min'];
-$max = $_POST['Max'];
+$colors = ['red', 'blue', 'green', 'yellow'];
+$number = 1;
+$currentColor = $colors[$_POST['color']] ?? $colors[$number];
+$size = $_GET['size'] ? $_GET['size'] + $_GET['size'] / 4 : 50;
+
+
 
 ?>
 
 <html>
 <body>
 
-<form action="index.php" method="post">
-    Min: <input type="text" name="Min"><br>
-    Max: <input type="text" name="Max"><br>
-    <input type="submit">
+<div style="width: <?php echo $size;?>px;height: <?php echo $size;?>px; background-color: <?php echo $currentColor;?>;"></div>
+<form action="index.php" id="form1" method="post">
+    <button type="submit" name="color" value="<?php echo $number?>">Change color</button>
+</form>
+<form action="index.php" id="form2" method="get">
+    <button type="submit" name="size" value="<?php echo $size;?>">Change size</button>
 </form>
 
-    Random number: <?php echo random_int($min, $max);?>
 
 </body>
 </html>
