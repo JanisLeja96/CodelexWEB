5968f6958a56413ccdc990db5a0cba7b3ddf3bd1
JanisLeja1996 <janis.leja1996@gmail.com>
2020-10-12 13:37:02 +0300

    Fixed styling

--- Safe/index.php
+++ Safe/index.php
@@ -2,7 +2,6 @@
 
 require_once 'Safe/Safe.php';
 session_start();
-$buttons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 $safe = new Safe(1234);
 
 if (isset($_POST['code'])) {
@@ -25,26 +24,31 @@ if (strlen($_SESSION['code']) >= 4) {
             height: 50%;
             margin-left: auto;
             margin-right: auto;
+            position: relative;
+            justify-content: center;
+            justify-items: center;
+            align-content: center;
+            align-items: center;
         }
 
         .buttons {
             width: 450px;
             height: 260px;
-            display: flex;
-            margin-left: auto;
-            margin-right: auto;
-            margin-bottom: auto;
-            align-content: space-around;
-            justify-content: center;
-            flex-wrap: wrap;
+            display: inline;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            -ms-transform: translate(-50%, -50%);
+            transform: translate(-50%, -50%);
+            text-align: center;
         }
 
         .button {
-
             height: 50px;
             width: 110px;
             margin: 5px;
             font-size: 24px;
+            display: inline;
         }
 
         .attempts {
@@ -78,10 +82,17 @@ if (strlen($_SESSION['code']) >= 4) {
 
 
     <div class="buttons">
-        <?php foreach ($buttons as $button) : ?>
-            <?= '<form method="post" action="/" name="code">'; ?>
-            <?= "<input type='submit' name='code' value='{$button}' class='button'></form>" ?>
-        <?php endforeach; ?>
+        <form method="post" action="/">
+            <button class="button" type="submit" name="code" value="1">1</button>
+            <button class="button" type="submit" name="code" value="2">2</button>
+            <button class="button" type="submit" name="code" value="3">3</button>
+            <button class="button" type="submit" name="code" value="4">4</button>
+            <button class="button" type="submit" name="code" value="5">5</button>
+            <button class="button" type="submit" name="code" value="6">6</button>
+            <button class="button" type="submit" name="code" value="7">7</button>
+            <button class="button" type="submit" name="code" value="8">8</button>
+            <button class="button" type="submit" name="code" value="9">9</button>
+        </form>
     </div>
 </div>
 
